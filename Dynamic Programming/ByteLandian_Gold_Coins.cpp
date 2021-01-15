b8c5ebe0079be0d27635579d9bb9d8361fbc57a0
Yacha Venkata Rakesh <venkatarakesh1234y@gmail.com>
2021-01-15 22:38:48 +0530

    My First Day Dynamic Programming

---
+++ Dynamic Programming/ByteLandian_Gold_Coins.cpp
@@ -0,0 +1,63 @@
+/*
+    Problem Link : https://www.spoj.com/problems/COINS/
+*/
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pl;
+typedef set<int> si;
+typedef set<int,greater<int>> sgi;
+typedef stack<int> sti;
+typedef stack<ll> stl;
+typedef unordered_set<int> usi;
+typedef unordered_set<ll> usl;
+typedef queue<int> qi;
+typedef queue<ll> ql;
+typedef unordered_map<int,int> umi;
+typedef unordered_map<ll,ll> uml;
+typedef unordered_map<int,ll> umil;
+typedef unordered_map<ll,int> umli;
+typedef map<int,int> mi;
+typedef map<ll,ll> ml;
+typedef map<int,ll> mil;
+typedef map<ll,int> mli;
+#define endl "\n"
+#define pb push_back
+#define forever for(;;)
+#define f(i,k,n) for(int i=k;i<n;i++)
+#define ef(i,k,n) for(int i=k;i<=n;i++)
+#define fl(i,k,n) for(ll i=k;i<n;i++)
+#define efl(i,k,n) for(ll i=k;i<=n;i++)
+#define vsort(v) sort(v.begin(),v.end())
+#define intscan(a) scanf("%d",&a)
+#define llscan(a) scanf("%lld",&a)
+#define println(a) printf("%d\n",a)
+#define llprintln(a) printf("%lld\n",a)
+#define prints(a) printf("%d ",a)
+#define llprints(a) printf("%lld ",a)
+#define coutln(a) cout<<a<<endl
+#define couts(a) cout<<a<<" "
+#define cout(a) cout<<a
+mil dp;
+ll func(int n){
+    if(n==1)
+        return 1;
+    if(n<=0)
+        return 0;
+    if(dp[n])
+        return dp[n];
+    return dp[n] = max(ll(n),func(n/2)+func(n/3)+func(n/4));
+}
+int main(){
+    ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+    int n;
+    forever{
+        if(intscan(n)!=1)
+            break;
+        llprintln(func(n));
+    }
+    return 0;
+} 
