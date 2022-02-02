c6a05253acb7db8e7d8faaa4bf051e7471e085f7
Yacha Venkata Rakesh <venkatarakesh1234y@gmail.com>
2022-02-02 07:56:29 +0530

    Non Decreasing Array - O(n) & O(1)

--- Data-Structures/Arrays/HackerEarth/02-Non-Decreasing-Arrays.cpp
+++ Data-Structures/Arrays/HackerEarth/02-Non-Decreasing-Arrays.cpp
@@ -10,8 +10,7 @@ using namespace std;
 typedef long long int ll;
 int main(){
 	ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-	cout.tie(NULL);
+    	cin.tie(NULL);
 	int t;
 	cin>>t;
 	for(int i=0;i<t;i++){
