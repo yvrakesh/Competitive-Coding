0dba08c1ff5b62b80fd5d59cde42386e9ac0f6db
yvrakesh <yachavenkatarakesh@gmail.com>
2022-02-27 20:33:41 +0530

    Is the number prime

---
+++ The-Bible-of-Coding-Interviews-And-Competitive-Programming/02-Mathematical-Fundamentals/01-Is-Prime/Naive.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool is_prime(int n){
+    if(n < 2)
+        return false;
+    if(n == 2)
+        return true;
+    if(!(n%2))
+        return false;
+    for(int i=3;i<=n;i+=2)
+        if(!(n%i))
+            return false;
+    return true;
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n;
+    cin>>n;
+    if(is_prime(n))
+        cout<<n<<" is a prime number\n";
+    else
+        cout<<n<<" is not a prime number\n";
+}
